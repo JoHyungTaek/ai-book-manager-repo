3d07c1a18e0005c27be403264cb2b9cd3ce88618
JHT <hyungtaek415@gmail.com>
2025-12-04 21:52:08 +0900

    프론트 Layout,HomePage 반영

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,17 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+// src/App.jsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Layout/Layout.jsx";
+import HomePage from "./pages/HomePage.jsx";
 
 function App() {
-    const [msg, setMsg] = useState("");
-
-    useEffect(() => {
-        axios.get("http://localhost:8080/api/hello")
-            .then((res) => setMsg(res.data))
-            .catch((err) => console.error(err));
-    }, []);
-
     return (
-        <div>
-            <h1>프론트엔드 - 백엔드 연동 테스트</h1>
-            <p>백엔드 응답: {msg}</p>
-        </div>
+        <BrowserRouter>
+            <Layout>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                </Routes>
+            </Layout>
+        </BrowserRouter>
     );
 }
 
---
+++ frontend/src/pages/homepage.jsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return (
+        <div>
+            <h1>홈페이지</h1>
+            <p>AI Book Manager에 오신 것을 환영합니다!</p>
+        </div>
+    );
+}
