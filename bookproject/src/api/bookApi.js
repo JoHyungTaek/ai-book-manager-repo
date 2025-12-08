d4e3276bcbf7deb0ad7aa89ab25c37026fd9a4bc
kimsojin <ksj16130@gmail.com>
2025-12-08 14:43:19 +0900

    user, book 프론트 연동쪽 수정

--- bookproject/src/api/bookApi.js
+++ bookproject/src/api/bookApi.js
@@ -6,6 +6,18 @@ const instance = axios.create({
     baseURL: "http://localhost:8080", // 백엔드 주소
 });
 
+// ✅ 모든 요청에 Authorization 헤더 자동 추가
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
 export default instance;
 
 // 도서 등록
